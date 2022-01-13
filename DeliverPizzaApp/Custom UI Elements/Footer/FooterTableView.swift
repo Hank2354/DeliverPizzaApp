2c50421802fc4a56b1f2e301ba878144eb0ed525
Vladislav <mashkovjob@yandex.ru>
2022-01-13 04:48:14 +0300

    Start install footer table view and create ProductModel

---
+++ DeliverPizzaApp/Custom UI Elements/Footer/FooterTableView.swift
@@ -0,0 +1,14 @@
+//
+//  FooterTableView.swift
+//  DeliverPizzaApp
+//
+//  Created by Vladislav Mashkov on 13.01.2022.
+//
+
+import UIKit
+
+class FooterTableView: UITableView {
+
+    
+
+}
---
+++ DeliverPizzaApp/Entities/ProductModel.swift
@@ -0,0 +1,15 @@
+//
+//  ProductModel.swift
+//  DeliverPizzaApp
+//
+//  Created by Vladislav Mashkov on 13.01.2022.
+//
+
+import Foundation
+
+struct ProductModel {
+    let imageURL: String?
+    let positionName: String
+    let positionDescription: String?
+    let minPrice: Decimal
+}
