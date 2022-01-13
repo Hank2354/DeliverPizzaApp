bcf2c6d6257fab4f9ffb0e2c27d416e72a949f9a
Vladislav <mashkovjob@yandex.ru>
2022-01-14 02:27:31 +0300

    update code layout

--- DeliverPizzaApp/Application/Menu/Extensions/MenuUITableViewDelegate.swift
+++ DeliverPizzaApp/Application/Menu/Extensions/MenuUITableViewDelegate.swift
@@ -13,34 +13,34 @@ extension MenuViewController: UITableViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
         // Get headerViewHeight and calsulate allowable y offset
-        let headerViewHeight = headerView.bounds.height
-        let allowableOffset = headerViewHeight - CGFloat(70)
+        let headerViewHeight =  headerView.bounds.height
+        let allowableOffset  =  headerViewHeight - CGFloat(70)
         
-        if let scrollView = scrollView as? FooterTableView {
+        if let scrollView    = scrollView as? FooterTableView {
             
             // get table and view offsets
-            let tableOffset = scrollView.contentOffset.y
-            let viewOffset  = mainScrollView.contentOffset.y
+            let tableOffset  = scrollView.contentOffset.y
+            let viewOffset   = mainScrollView.contentOffset.y
             
             if viewOffset < allowableOffset || tableOffset < 0 {
 
-                let currentScrollViewOffset = mainScrollView.contentOffset.y
-                let currentTableViewOffset = scrollView.contentOffset.y
+                let currentScrollViewOffset    =  mainScrollView.contentOffset.y
+                let currentTableViewOffset     =  scrollView.contentOffset.y
                 
-                let newScrollViewOffset = min((currentScrollViewOffset + tableOffset), (allowableOffset + 1))
-                let newTableViewOffset = min((currentTableViewOffset - tableOffset), allowableOffset)
+                let newScrollViewOffset        =  min((currentScrollViewOffset + tableOffset), (allowableOffset + 1))
+                let newTableViewOffset         =  min((currentTableViewOffset - tableOffset), allowableOffset)
                 
-                scrollView.contentOffset.y = newTableViewOffset
-                mainScrollView.contentOffset.y = newScrollViewOffset
+                scrollView.contentOffset.y     =  newTableViewOffset
+                mainScrollView.contentOffset.y =  newScrollViewOffset
 
             }
             
             // Show header menu if user want
             if viewOffset < -10 || tableOffset < -1 {
                 
-                mainScrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
-                footerTableView.isScrollEnabled = false
-                footerTableView.isScrollEnabled = true
+                mainScrollView  .setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+                footerTableView .isScrollEnabled = false
+                footerTableView .isScrollEnabled = true
                 
             }
             
